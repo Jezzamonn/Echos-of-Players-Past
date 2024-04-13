4ef41f84f1c76a3eb37ad1a87178b8698403b9fb
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2024-04-12 20:00:22 -0700

    Add stuff from my base project that I almost forgot about

--- client/build/js/main.bundle.js
+++ client/build/js/main.bundle.js
@@ -1,12 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./ts/client.ts ***!
-  \**********************/
-
-console.log('Hello, world!');
-
+/******/ 	
+/******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=main.bundle.js.map
---
+++ client/ts/game/sfx.ts
@@ -0,0 +1,27 @@
+import { sfxr } from "jsfxr";
+import { MuteState, Sounds } from "../lib/sounds";
+
+const sfx = {};
+
+class _SFX {
+    sounds: { [key: string]: any } = {};
+
+    preload() {
+        for (let key in sfx) {
+            this.sounds[key] = sfxr.toAudio(sfx[key]);
+        }
+    }
+
+    play(name: string) {
+        if (Sounds.muteState === MuteState.ALL_OFF) {
+            return;
+        }
+
+        const sound = this.sounds[name];
+        if (sound) {
+            sound.play();
+        }
+    }
+}
+
+export const SFX = new _SFX();
